702dedcaf3906e6799cdbf512bb03dfe82db1c39
LarisaPobol <larisapobol@gmail.com>
2019-03-07 10:04:27 +0300

    Add files via upload

---
+++ tracertlab/tracertlab/ICMP.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tracertlab
+{
+    class ICMP
+    {
+        public byte type;
+        public byte code;
+        public UInt16 checksum;
+        public int mesLength;
+        public byte[] Message = new byte[1024];
+
+        public ICMP()
+        {
+        }
+        ~ICMP() { }
+        public ICMP(int length, byte[] message)
+        {
+            type = message[20];
+            code = message[21];
+            checksum = BitConverter.ToUInt16(message, 22);
+            mesLength = length - 24;
+            Buffer.BlockCopy(message, 24, Message, 0, mesLength);
+        }
+
+        public byte[] getBytes()
+        {
+            byte[] msg = new byte[mesLength + 9];
+            Buffer.BlockCopy(BitConverter.GetBytes(type), 0, msg, 0, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(code), 0, msg, 1, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(checksum), 0, msg, 2, 2);
+            Buffer.BlockCopy(Message, 0, msg, 4, mesLength);
+            return msg;
+        }
+
+        public UInt16 CountedChecksum()
+        {
+            UInt32 currChecksum = 0;
+            byte[] msg = getBytes();
+            int packetsize = mesLength + 8;
+            int index = 0;
+
+            while (index < packetsize)
+            {
+                currChecksum += Convert.ToUInt32(BitConverter.ToUInt16(msg, index));
+                index += 2;
+            }
+            currChecksum = (currChecksum >> 16) + (currChecksum & 0xffff);
+            currChecksum += (currChecksum >> 16);
+            return (UInt16)(~currChecksum);
+        }
+        public int CreateICMPrequest(byte[] msg,  short number)
+        {
+            type = 8;//эхо-запрос
+            code = 0;
+            checksum = 0;
+            Buffer.BlockCopy(BitConverter.GetBytes(1), 0, Message, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(number), 0, Message, 2, 2);
+            Buffer.BlockCopy(msg, 0, Message, 4, msg.Length);
+            mesLength = msg.Length + 4;
+            checksum = CountedChecksum();
+           return mesLength + 4;
+        }
+
+        public void changeIdNumber(short number)
+        {
+            checksum = 0;
+            Buffer.BlockCopy(BitConverter.GetBytes(number), 0, Message, 2, 2);
+            checksum = CountedChecksum();
+        }
+    }
+}
---
+++ tracertlab/tracertlab/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tracertlab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short number = 1;
+            short id = 1;
+            const string teststring = "1234";//строка, которая будет отправляться
+            const int mesSize = 1024;                               
+            string ipAddr = args[0];
+            bool getDomen = false;
+            bool isRIgthRecv = false;
+       if ((args.Length==2)&&(args[1]=="getDomainName"))
+            {
+                getDomen = true;
+            }
+            //Console.WriteLine("enter ip-adress");
+           // string ipAddr = Console.ReadLine();
+            IPAddress iP;
+            IPEndPoint iep;
+            bool isIP = IPAddress.TryParse(ipAddr,out iP);
+            if (isIP)
+            {
+                iep = new IPEndPoint(IPAddress.Parse(ipAddr), 0);
+            }
+            else
+            {
+                IPHostEntry iphe = Dns.Resolve(ipAddr);
+                iep = new IPEndPoint(iphe.AddressList[0], 0);
+            }
+
+            byte[] message = new byte[mesSize];
+            message = Encoding.ASCII.GetBytes(teststring);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            EndPoint ep = (EndPoint)iep;
+            ICMP packet = new ICMP();
+            int ICMPsize;
+            int recv;
+            ICMPsize = packet.CreateICMPrequest(message, number);//создаем ICMP-пакет для запроса
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
+            Console.WriteLine("Tracing route to {0}",iep.ToString() );
+            short ttl = 1;
+            int start, end;
+            bool epIsReached = false;
+            do 
+            {
+                Console.Write("{0}", ttl);
+                for ( int i=0; i<3; i++) 
+                {
+                    packet.changeIdNumber(number);
+                    socket.Ttl = ttl;
+                    start = Environment.TickCount;
+                    socket.SendTo(packet.getBytes(), ICMPsize, SocketFlags.None, iep);                   
+                    try
+                    {
+                        message = new byte[mesSize];
+                        recv = socket.ReceiveFrom(message, ref ep);
+                        ICMP reply = new ICMP(recv, message);
+                       // Console.Write(" {0} ", reply.getNum().ToString());
+                        end = Environment.TickCount;                       
+                        if (reply.type == 11)
+                        {
+                            Console.Write(" {0}ms ", end - start);
+                            if (i == 2) {
+                                Console.Write(" {0}  ", ep.ToString());
+
+                                if (getDomen)
+                                {
+                                    IPHostEntry domen;
+                                    try
+                                    {
+                                        IPEndPoint ip = (IPEndPoint)ep;
+                                        IPAddress ip1 = ip.Address;
+                                        domen = Dns.GetHostEntry(ip1.ToString());
+                                        Console.Write("[ {0} ]", domen.HostName);
+                                    }
+                                    catch
+                                    {
+                                        Console.Write("-");
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (reply.type ==0)
+                            {
+                                epIsReached = true;
+                                Console.Write(" {0}ms ", end - start);
+                                if (i == 2) {
+                                    Console.Write(" {0} ", ep.ToString());
+
+                                    if (getDomen)
+                                    {
+                                        IPHostEntry domen;
+                                        try
+                                        {
+                                            IPEndPoint ip = (IPEndPoint)ep;
+                                            IPAddress ip1 = ip.Address;
+                                            domen = Dns.GetHostEntry(ip1.ToString());
+                                            Console.Write("[ {0} ]", domen.HostName);
+                                        }
+                                        catch
+                                        {
+                                            Console.Write("-");
+                                        }
+                                    }
+
+                                }
+                                                               
+                            }
+                        }
+
+                    }
+                    catch (SocketException)
+                    {
+                        Console.Write("* ");
+                    }
+                    number++;
+                }
+                Console.WriteLine();
+                ttl++;
+                id++;
+            }while ((ttl<30)&&(!epIsReached));
+
+            if (epIsReached)
+            {
+                Console.WriteLine("Trace complete");
+            }
+
+            socket.Close();
+
+        }
+    }
+}
